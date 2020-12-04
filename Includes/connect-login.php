84dd8dfbd9087129abefd6bdf8f6305b8a13678f
MeLIoDs21 <loal00001@utb.vaxjo.se>
2020-12-04 12:03:33 +0100

    Add files via upload

---
+++ Includes/connect-login.php
@@ -0,0 +1,72 @@
+<?php
+
+	$dbh = new mysqli("localhost", "MeLIoDs", "loal00001", "webbshop");
+
+	if(!$dbh){
+		echo "Ingen kontakt med databasen";
+		exit;
+	}
+	$sql = "SELECT password FROM members WHERE Username=?";
+	$res = $dbh->prepare($sql);			// Prepar qustion
+	$res->bind_param("s", $username);						// Asks the qustione
+	$res->execute();						// Asks the qustione
+	$result = $res->get_result();			// Brings results
+	$row = $result -> fetch_assoc();
+
+	if(!$result){
+		echo "Felaktig SQL fråga";
+		exit;
+	}
+
+	$dbh ->close();							// Closes the database
+
+	if($row["password"] == $password){
+		session_start();
+		$_SESSION['Username']= $username;
+		header("Location: admin.php");
+	} else { echo "Fail"; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+header('Content-Type: text/html, charset=utf-8');
+
+$dbh = new mysqli("localhost", "MeLIoDs", "loal00001", "webbshop");
+
+if(!$dbh){
+	echo "Ingen kontakt med databasen";
+	exit;
+}
+
+$sql = "SELECT Username FROM members ";
+$res = $dbh->prepare($sql);			// Prepar qustion
+$res->execute();						// Asks the qustione
+$result = $res->get_result();			// Brings results
+
+if(!$result){
+	echo "Felaktig SQL fråga";
+	exit;
+}
+
+$dbh ->close();							// Closes the database
+
+while($row = $result->fetch_assoc()){
+	if($row['Username'] == $username)
+		echo "Lul";
+
+}
+*/
--- html/login2.php
+++ html/login2.php
@@ -1,39 +1,21 @@
 <?php
-
 	 header('Content-Type: text/html, charset=utf-8');
-	 
-	 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-	 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-	 
-
-	$dbh = new mysqli("localhost", "MeLIoDs", "loal00001", "webbshop");
-
-	if(!$dbh){
-		echo "Ingen kontakt med databasen";
-		exit;
-	}
-
-	$sql = "SELECT Username, password FROM members WHERE Username='{$username}'";
-	$res = $dbh->prepare($sql);			// Prepar qustion
-	$res->execute();						// Asks the qustione
-	$result = $res->get_result();			// Brings results
-
-	if(!$result){
-		echo "Felaktig SQL fråga";
-		exit;
-	}
-
-	$dbh ->close();							// Closes the database
-
-	if($result -> fetch_assoc()["password"] == "{$password}"){
-		echo	"Succe";
-		echo	"<br>";
-	} else { echo "Fail"; }
 	
-	/*
-			while($row = $result->fetch_assoc()){
-			echo	$row['prise']." kronor for one " . $row['username'];
-			echo	"<br>";
-	 */
-	 
-	 
+	
+	if(empty($_POST['username']) || empty($_POST['password'])){
+		header("Location: login.php");
+	}else{
+		$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		
+		require "../Includes/connect-login.php";
+		
+	}
+		
+		/*
+				while($row = $result->fetch_assoc()){
+				echo	$row['prise']." kronor for one " . $row['username'];
+				echo	"<br>";
+		 */
+		 
+		 
